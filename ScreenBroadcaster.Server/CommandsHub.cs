7efc48810d71791e3e56bb089be56a50dac66bed
IvMykh <iv.mykh@gmail.com>
2016-04-10 20:02:34 +0300

    Previous commit with some bugs 2: added untracked new files.

---
+++ ScreenBroadcaster.Server/CommandsHub.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+
+using Microsoft.AspNet.SignalR;
+
+using ScreenBroadcaster.Common;
+
+namespace ScreenBroadcaster.Server
+{
+    public class CommandsHub
+        : Hub
+    {
+        private IDictionary<User, List<User>>                   _broadcasterReceiversDictionary;
+        private IDictionary<
+            ClientToServerCommand, Action<CommandParameter>>    _handlers;
+
+        public CommandsHub()
+        {
+            _broadcasterReceiversDictionary = new Dictionary<User, List<User>>();
+            _handlers                       = setupHandlers();
+
+            Debug.WriteLine("CommandsHub initialized.");
+        }
+
+        private IDictionary<
+            ClientToServerCommand, Action<CommandParameter>> setupHandlers()
+        {
+            var handlers = new Dictionary<ClientToServerCommand, Action<CommandParameter>>();
+
+            handlers[ClientToServerCommand.RegisterNewBroadcaster] = 
+                new Action<CommandParameter>((param) =>
+                    {
+                        _broadcasterReceiversDictionary[(User)param["user"]] = new List<User>();
+                        Clients.Caller.ExecuteCommand(ServerToClientCommand.ReportSuccessfulBcasterRegistration, null);
+                    });
+
+            return handlers;
+        }
+
+        public void ExecuteCommand(ClientToServerCommand command, CommandParameter argument)
+        {
+            _handlers[command](argument);
+        }
+
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+
+        public override Task OnConnected()
+        {
+            //Use Application.Current.Dispatcher to access UI thread from outside the MainWindow class
+            Application.Current.Dispatcher.Invoke(() =>
+                ((ServerMainWindow)Application.Current.MainWindow).ServerController.WriteToConsole("Client connected: " + Context.ConnectionId));
+
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected()
+        {
+            //Use Application.Current.Dispatcher to access UI thread from outside the MainWindow class
+            Application.Current.Dispatcher.Invoke(() =>
+                ((ServerMainWindow)Application.Current.MainWindow).ServerController.WriteToConsole("Client disconnected: " + Context.ConnectionId));
+
+            return base.OnDisconnected();
+        }
+    }
+}
