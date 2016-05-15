74a2f1a4a481b5384ccf714b8928b72b39eb8ab5
IvMykh <iv.mykh@gmail.com>
2016-05-15 22:17:01 +0300

    Changed ports

--- ScreenBroadcaster.Server/Controllers/ServerController.cs
+++ ScreenBroadcaster.Server/Controllers/ServerController.cs
@@ -14,7 +14,7 @@ namespace ScreenBroadcaster.Server.Controllers
     public class ServerController
     {
         // Constants.
-        const string                    SERVER_URI = "http://localhost:8080";
+        const string                    SERVER_URI = "http://localhost:3000";
 
         // Instanse members.
         public IDisposable      SignalR             { get; private set; }
